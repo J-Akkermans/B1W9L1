6be5d6c487c95386de7deb25405e53d06e1cc5aa
J Akkermans <99044668@mydavinci.nl>
2020-10-06 00:45:16 +0200

    added some functionality.

--- Opdracht4/opdracht4.js
+++ Opdracht4/opdracht4.js
@@ -1,12 +1,19 @@
 var num = prompt("Cijfer");
 var num1 = parseInt(num);
 
-function tafels(num1){
-    for(var i = 0; i < 11; i++){
-        console.log(i);
-        
-        document.write(num1 * i + " ")
+function tafels(num1) {
+    if (num1 <= 10) {
+        for (var x = 1; x <= num1; x++) {
+            for (var i = 1; i <= 10; i++) {
+                document.write(num1 * i + " ")
+            }
+        }
+    }
+    else{
+        alert("Je kan alleen tafels tot 10 laten zien..");
+        location.reload();
     }
 }
+
 
 tafels(num1)
